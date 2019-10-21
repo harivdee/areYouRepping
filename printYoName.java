5db8cb72edfc06c673d2a43d82d29132466c0092
George L <georgel.1986@gmail.com>
2019-10-21 13:35:40 +0300

    Add files via upload

---
+++ printYoName.java
@@ -0,0 +1,13 @@
+class printYoName {
+//import java.util.Scanner;
+	public static void main(String[] args) {
+		/*
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What's yo name?");
+		String name = sc.next();
+		System.out.println("Yo name is "+name+".");
+		*/
+		System.out.println("Yo");
+	}
+
+}
